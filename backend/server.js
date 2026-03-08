b8d5312f7a033e9cb09d1a4c22ce7ea03c7b5763
Sanika Shinde <shindesanika900@gmail.com>
2026-03-08 09:33:49 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://groceryadmin:grocery123@cluster0.lrmkhep.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let products; // global collection
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB Atlas");
+
+    const db = client.db("groceryDB");
+    products = db.collection("products");
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Grocery Cloud Server Running");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const data = await products.find().toArray();
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching products");
+  }
+});
+
+const { ObjectId } = require("mongodb");
+
+app.delete("/products/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const result = await products.deleteOne({
+    _id: new ObjectId(id)
+  });
+
+  res.send(result);
+}); 
+
+app.post("/products", async (req, res) => {
+  try {
+    const newProduct = req.body;
+    const result = await products.insertOne(newProduct);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error adding product");
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
---
+++ frontend/script.js
@@ -0,0 +1,102 @@
+function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        alert("Please fill all fields");
+        return;
+    }
+
+    window.location.href = "dashboard.html";
+}
+
+let total = 0;
+let completed = 0;
+
+async function loadItems() {
+    const res = await fetch("http://localhost:5000/products");
+    const data = await res.json();
+
+    const list = document.getElementById("groceryList");
+    list.innerHTML = "";
+
+    total = 0;
+    completed = 0;
+
+    data.forEach(item => {
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            <div class="item-left">
+                <input type="checkbox" onchange="toggleComplete(this)">
+                <span>${item.name}</span>
+            </div>
+            <button onclick="deleteItem(this, '${item._id}')">Delete</button>
+        `;
+
+        list.appendChild(li);
+        total++;
+    });
+
+    updateStats();
+}
+
+async function addItem() {
+    let input = document.getElementById("itemInput");
+    let value = input.value.trim();
+
+    if (value === "") {
+        alert("Enter an item");
+        return;
+    }
+
+    await fetch("http://localhost:5000/products", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: value,
+            price: 0
+        })
+    });
+
+    input.value = "";
+    loadItems();
+}
+
+function toggleComplete(checkbox) {
+    let span = checkbox.nextElementSibling;
+
+    if (checkbox.checked) {
+        span.classList.add("done");
+        completed++;
+    } else {
+        span.classList.remove("done");
+        completed--;
+    }
+
+    updateStats();
+}
+
+async function deleteItem(button, id) {
+    await fetch(`http://localhost:5000/products/${id}`, {
+        method: "DELETE"
+    });
+
+    button.parentElement.remove();
+    total--;
+    updateStats();
+}
+
+function updateStats() {
+    document.getElementById("totalCount").innerText = total;
+    document.getElementById("completedCount").innerText = completed;
+    document.getElementById("pendingCount").innerText = total - completed;
+}
+
+function logout() {
+    window.location.href = "index.html";
+}
+
+window.onload = loadItems;
